8319cade286e7c3a7198b300b20c571bbc623e90
KhanhAnFake <khanhanfake.pham@gmail.com>
2024-03-10 11:32:57 +0700

    add header n footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,51 @@
-import React from "react";
+import { footerLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
+      <div className="flex max-md:flex-col flex-wrap justify-center gap-5 sm:px-16 px-6 py-10">
+        <div className="flex flex-col justify-start items-start gap-6">
+          <Image
+            src="/logo.svg"
+            alt="logo"
+            width={118}
+            height={18}
+            className=" object-contain"
+          />
+          <p className="text-base text-gray-700">CarSpace</p>
+        </div>
+        <div className="footer__links">
+          {footerLinks.map((link) => (
+            <div key={link.title} className="footer__link">
+              <h3 className="font-bold">{link.title}</h3>
+              {link.links.map((item) => (
+                <Link
+                  key={item.title}
+                  href={item.url}
+                  className="text-gray-500"
+                >
+                  {item.title}
+                </Link>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
+        <p>@2024 CarSpace &copy; Pham Khanh An</p>
+        <div className="footer__copyrights-link">
+          <Link href="/" className="text-gray-500">
+            Privacy Policy
+          </Link>
+          <Link href="/" className="text-gray-500">
+            Terms of Use
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
